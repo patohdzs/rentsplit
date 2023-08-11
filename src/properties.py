405748ee88706e06dca81386b3e177326da3a8b6
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2023-08-11 19:13:24 +0100

    Fixed typing issues with PE check

--- src/properties.py
+++ src/properties.py
@@ -1,12 +1,8 @@
-from typing import Iterable
-
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
 
-def is_envy_free(
-    mu: Iterable[int], p: np.ndarray, V: np.ndarray, verbose: bool = False
-):
+def is_envy_free(mu: tuple[int], p: np.ndarray, V: np.ndarray, verbose: bool = False):
     for i, m in enumerate(mu):
         for j, n in enumerate(mu):
             ef_cond = V[i][m] - p[m] >= V[i][n] - p[n]
@@ -20,7 +16,7 @@ def is_envy_free(
     return True
 
 
-def is_efficient(mu: Iterable[int], V: np.ndarray) -> bool:
+def is_efficient(mu: tuple[int], V: np.ndarray) -> bool:
     n = V.shape[0]
     i, j = linear_sum_assignment(V, maximize=True)
     social_surplus = V[i, j].sum()
