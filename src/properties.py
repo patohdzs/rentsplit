d39d559fe3f21a9e0798246be84c94ed5cd2c343
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2023-08-12 02:28:43 +0100

    Added docstrings to properties module

--- src/properties.py
+++ src/properties.py
@@ -2,7 +2,20 @@ import numpy as np
 from scipy.optimize import linear_sum_assignment
 
 
-def is_envy_free(mu: tuple[int], p: np.ndarray, V: np.ndarray, verbose: bool = False):
+def is_envy_free(
+    mu: tuple[int], p: np.ndarray, V: np.ndarray, verbose: bool = False
+) -> bool:
+    """Checks if an allocation is envy-free.
+
+    Args:
+        mu (tuple[int]): Allocation room assignment.
+        p (np.ndarray): Allocation room prices.
+        V (np.ndarray): Matrix of room valuations.
+        verbose (bool, optional): Verbosity. Defaults to False.
+
+    Returns:
+        bool: True for EF allocations.
+    """
     for i, m in enumerate(mu):
         for j, n in enumerate(mu):
             ef_cond = V[i][m] - p[m] >= V[i][n] - p[n]
@@ -17,6 +30,15 @@ def is_envy_free(mu: tuple[int], p: np.ndarray, V: np.ndarray, verbose: bool = F
 
 
 def is_efficient(mu: tuple[int], V: np.ndarray) -> bool:
+    """Checks if a room assignment is Pareto-efficient.
+
+    Args:
+        mu (tuple[int]): Room assignment to check.
+        V (np.ndarray): Matrix of room valuations.
+
+    Returns:
+        bool: True for PE assignments.
+    """
     n = V.shape[0]
     i, j = linear_sum_assignment(V, maximize=True)
     social_surplus = V[i, j].sum()
