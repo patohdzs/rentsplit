f8a22b46fd858cd3b867c62a950716740534f936
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2023-08-18 10:48:31 +0100

    Implemented discrete auction mechanism.

--- src/pricing.py
+++ src/pricing.py
@@ -19,7 +19,6 @@ def compute_auction_prices(V: np.ndarray, R: int, tol: int = 10**5) -> np.ndarra
     """
     # Initialize variables
     n = V.shape[0]
-    delta = 1 / (8 * n)
     p = (R / n) * np.ones(n)
     t = 0
     while t < tol:
@@ -30,16 +29,41 @@ def compute_auction_prices(V: np.ndarray, R: int, tol: int = 10**5) -> np.ndarra
             p = p + (R - p.sum()) / n
             return p
 
+        # Find increment
+        x = _find_increment(V, p)
+
         # Increase prices of over demanded
-        p[od] += delta
+        p[od] += ((n - len(od)) / n) * x
 
         # Decrease prices of not-over-demanded
         not_od = np.setdiff1d(range(n), od)
-        p[not_od] -= (len(od) / (n - len(od))) * delta
+        p[not_od] -= (len(od) / n) * x
 
         # Increase counter
         t += 1
     raise Exception(f"TIMEOUT after {tol} iterations.")
+
+
+def _find_increment(V, p):
+    # Utility
+    U = V - p
+
+    # Utility tilde (max utility)
+    max_u = np.max(U, axis=1)
+
+    # Overdemanded and not overdemanded rooms
+    od = _find_overdemanded(V, p)
+    not_od = np.setdiff1d(range(V.shape[0]), od)
+
+    # Find J
+    D = _demands(V, p)
+    J = [i for i in range(V.shape[0]) if np.isin(D[D[:, 0] == i, 1], od).all()]
+
+    # Inner part of the problem
+    inner = max_u - np.take(U, not_od, axis=1).max(axis=1)
+
+    # Return x
+    return inner[J].min()
 
 
 def _find_overdemanded(V: np.ndarray, p: np.ndarray):
@@ -48,7 +72,7 @@ def _find_overdemanded(V: np.ndarray, p: np.ndarray):
     carryon = True
 
     while carryon:
-        mask = _iteration(D)
+        mask = _is_underdemanded(D)
         if mask is not None:
             D = D[~mask, :]
         else:
@@ -57,7 +81,7 @@ def _find_overdemanded(V: np.ndarray, p: np.ndarray):
     return np.unique(D[:, 1])
 
 
-def _iteration(D: np.ndarray) -> np.ndarray | None:
+def _is_underdemanded(D: np.ndarray) -> np.ndarray | None:
     # Get unique and counts
     unique, counts = np.unique(D[:, 1], return_counts=True)
 
